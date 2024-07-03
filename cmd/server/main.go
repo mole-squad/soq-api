7b1f4feb601f9e4c36048410767b31eb7e62152c
Burke Livingston <burkel24@gmail.com>
2024-07-03 10:55:00 -0700

    separate out seeder

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,73 +1,15 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net"
-	"net/http"
-
-	"github.com/burkel24/task-app/pkg/db"
-	"github.com/burkel24/task-app/pkg/interfaces"
-	"github.com/burkel24/task-app/pkg/models"
-	"github.com/burkel24/task-app/pkg/tasks"
-	"github.com/burkel24/task-app/pkg/users"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/render"
+	"github.com/burkel24/task-app/pkg/app"
 	"go.uber.org/fx"
 )
 
-const port = ":3000"
-
-func NewRouter() *chi.Mux {
-	router := chi.NewRouter()
-	router.Use(middleware.DefaultLogger)
-	router.Use(render.SetContentType(render.ContentTypeJSON))
-
-	router.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("okay xD"))
-	})
-
-	return router
-}
-
-func NewServer(lc fx.Lifecycle, router *chi.Mux) *http.Server {
-	srv := &http.Server{Addr: port, Handler: router}
-
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			ln, err := net.Listen("tcp", srv.Addr)
-			if err != nil {
-				return err
-			}
-
-			fmt.Println("Starting HTTP server at", srv.Addr)
-			go srv.Serve(ln)
-
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			fmt.Println("Shutting down")
-
-			return srv.Shutdown(ctx)
-		},
-	})
-
-	return srv
-}
-
-func NewSeeder(lc fx.Lifecycle, dbService interfaces.DBService) {
-	dbService.CreateOne(context.Background(), &models.User{})
-}
-
 func main() {
-	fx.New(
-		db.Module,
-		users.Module,
-		tasks.Module,
-		fx.Provide(NewRouter),
-		fx.Provide(NewServer),
-		fx.Invoke(func(*http.Server) {}),
-		fx.Invoke(NewSeeder),
-	).Run()
+	appOpts := app.BuildAppOpts()
+	serverOpts := app.BuildServerOpts()
+
+	allOpts := append(appOpts, serverOpts...)
+
+	fx.New(allOpts...).Run()
 }
---
+++ pkg/app/app.go
@@ -0,0 +1,71 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/burkel24/task-app/pkg/db"
+	"github.com/burkel24/task-app/pkg/tasks"
+	"github.com/burkel24/task-app/pkg/users"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"go.uber.org/fx"
+)
+
+const port = ":3000"
+
+func NewRouter() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.DefaultLogger)
+	router.Use(render.SetContentType(render.ContentTypeJSON))
+
+	router.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("okay xD"))
+	})
+
+	return router
+}
+
+func NewServer(lc fx.Lifecycle, router *chi.Mux) *http.Server {
+	srv := &http.Server{Addr: port, Handler: router}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go srv.Serve(ln)
+
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			fmt.Println("Shutting down")
+
+			return srv.Shutdown(ctx)
+		},
+	})
+
+	return srv
+}
+
+func BuildServerOpts() []fx.Option {
+	return []fx.Option{
+		fx.Provide(NewServer),
+		fx.Invoke(func(*http.Server) {}),
+	}
+}
+
+func BuildAppOpts() []fx.Option {
+	return []fx.Option{
+		fx.Provide(NewRouter),
+		db.Module,
+		users.Module,
+		tasks.Module,
+	}
+}
