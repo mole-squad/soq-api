1b27b317d18bdcafaa572382376584b5e08488ac
Burke Livingston <burkel24@gmail.com>
2024-07-12 11:43:01 -0700

    feat: add set password API (#27)

--- pkg/api/usercontroller.go
+++ pkg/api/usercontroller.go
@@ -1,8 +1,14 @@
 package api
 
 import (
+	"net/http"
+
+	"github.com/burkel24/task-app/pkg/auth"
+	"github.com/burkel24/task-app/pkg/common"
 	"github.com/burkel24/task-app/pkg/interfaces"
+	"github.com/burkel24/task-app/pkg/users"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"go.uber.org/fx"
 )
 
@@ -30,9 +36,35 @@ func NewUserController(params UserControllerParams) (UserControllerResult, error
 	userRouter := chi.NewRouter()
 	userRouter.Use(params.AuthService.AuthRequired())
 
+	userRouter.Patch("/password", ctrl.SetPassword)
+
 	params.Router.Mount("/users", userRouter)
 
 	return UserControllerResult{
 		UserController: ctrl,
 	}, nil
+}
+
+func (ctrl *UserController) SetPassword(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	user, err := auth.GetUserFromCtx(ctx)
+	if err != nil {
+		render.Render(w, r, common.ErrUnauthorized(err))
+		return
+	}
+
+	dto := &users.SetPasswordRequestDTO{}
+	if err := render.Bind(r, dto); err != nil {
+		render.Render(w, r, common.ErrInvalidRequest(err))
+		return
+	}
+
+	err = ctrl.userService.UpdateUserPassword(ctx, user.ID, dto.Password)
+	if err != nil {
+		render.Render(w, r, common.ErrUnknown(err))
+		return
+	}
+
+	render.Status(r, http.StatusNoContent)
 }
--- pkg/interfaces/userservice.go
+++ pkg/interfaces/userservice.go
@@ -10,4 +10,5 @@ type UserService interface {
 	ListUsers(ctx context.Context) ([]models.User, error)
 	GetUserByID(ctx context.Context, userID uint) (*models.User, error)
 	GetUserByCredentials(ctx context.Context, username, passwordHash string) (*models.User, error)
+	UpdateUserPassword(ctx context.Context, userID uint, password string) error
 }
