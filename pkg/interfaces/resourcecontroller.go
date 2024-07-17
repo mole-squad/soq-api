4814b38213dd1732e97d69c3640a81e1c22da1a4
Burke Livingston <burkel24@gmail.com>
2024-07-17 10:16:44 -0700

    feat: consolidate generic patterns (#40)
    * feat: consolidate generic patterns
    
    * untracked file

---
+++ pkg/interfaces/resourcecontroller.go
@@ -0,0 +1,22 @@
+package interfaces
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type ResourceController[M Resource] interface {
+	List(w http.ResponseWriter, r *http.Request)
+	Create(w http.ResponseWriter, r *http.Request)
+	Get(w http.ResponseWriter, r *http.Request)
+	Update(w http.ResponseWriter, r *http.Request)
+	Delete(w http.ResponseWriter, r *http.Request)
+
+	ItemFromContext(ctx context.Context) (M, error)
+	ItemContextMiddleware(next http.Handler) http.Handler
+	UserAccessMiddleware(next http.Handler) http.Handler
+
+	GetRouter() *chi.Mux
+}
