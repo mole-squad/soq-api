12ed7437c5614367ffd468227117cbc8dc54ff3a
Burke Livingston <burkel24@gmail.com>
2024-07-17 20:50:17 -0700

    feat: add timewindows APIs

--- pkg/models/timewindow.go
+++ pkg/models/timewindow.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"net/http"
+
+	"github.com/go-chi/render"
 	"github.com/lib/pq"
 	"github.com/mole-squad/soq-api/api"
 	"gorm.io/gorm"
@@ -21,7 +24,19 @@ type TimeWindow struct {
 	User   User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
-func (t *TimeWindow) ToDTO() *api.TimeWindowDTO {
+func (t *TimeWindow) GetID() uint {
+	return t.ID
+}
+
+func (t *TimeWindow) GetUserID() uint {
+	return t.UserID
+}
+
+func (t *TimeWindow) SetUserID(userID uint) {
+	t.UserID = userID
+}
+
+func (t *TimeWindow) ToDTO() render.Renderer {
 	dto := &api.TimeWindowDTO{
 		ID:        t.ID,
 		Weekdays:  t.Weekdays,
@@ -30,4 +45,35 @@ func (t *TimeWindow) ToDTO() *api.TimeWindowDTO {
 	}
 
 	return dto
+}
+
+func NewTimeWindowFromCreateRequest(r *http.Request) (*TimeWindow, error) {
+	timeWindow := &TimeWindow{}
+
+	dto := &api.CreateTimeWindowRequestDTO{}
+	if err := render.Bind(r, dto); err != nil {
+		return nil, err
+	}
+
+	timeWindow.Weekdays = dto.Weekdays
+	timeWindow.StartTime = dto.StartTime
+	timeWindow.EndTime = dto.EndTime
+	timeWindow.FocusAreaID = dto.FocusAreaID
+
+	return timeWindow, nil
+}
+
+func NewTimeWindowFromUpdateRequest(r *http.Request) (*TimeWindow, error) {
+	timeWindow := &TimeWindow{}
+
+	dto := &api.UpdateTimeWindowRequestDTO{}
+	if err := render.Bind(r, dto); err != nil {
+		return nil, err
+	}
+
+	timeWindow.Weekdays = dto.Weekdays
+	timeWindow.StartTime = dto.StartTime
+	timeWindow.EndTime = dto.EndTime
+
+	return timeWindow, nil
 }
