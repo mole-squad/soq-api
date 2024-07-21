b61eb32748d34b35bf29c1fa1488124d318bbb29
Burke Livingston <burkel24@gmail.com>
2024-07-21 10:13:43 -0700

    feat: use patch instead of post for resolve task

--- api/createtimewindowrequestdto.go
+++ api/createtimewindowrequestdto.go
@@ -1,14 +1,30 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type CreateTimeWindowRequestDTO struct {
-	Weekdays    []int32 `json:"weekdays"`
-	StartTime   float32 `json:"startTime"`
-	EndTime     float32 `json:"endTime"`
-	FocusAreaID uint    `json:"focusAreaId"`
+	Weekdays       []int32     `json:"weekdays"`
+	StartTime      float32     `json:"startTime"`
+	EndTime        float32     `json:"endTime"`
+	FocusAreaIDNum json.Number `json:"focusAreaId"`
+	FocusAreaID    uint
 }
 
 func (dto *CreateTimeWindowRequestDTO) Bind(r *http.Request) error {
+	if dto.FocusAreaIDNum.String() == "" {
+		return nil
+	}
+
+	focusAreaID, err := dto.FocusAreaIDNum.Int64()
+	if err != nil {
+		return fmt.Errorf("failed to parse focusAreaId: %w", err)
+	}
+
+	dto.FocusAreaID = uint(focusAreaID)
+
 	return nil
 }
