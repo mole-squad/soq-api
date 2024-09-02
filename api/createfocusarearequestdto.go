90551e86043c514b24641d7706ab9b8ebb6a81b9
Burke Livingston <burkel24@gmail.com>
2024-09-02 12:10:56 -0700

    feat: validate name on focus area create (#47)

--- api/createfocusarearequestdto.go
+++ api/createfocusarearequestdto.go
@@ -1,11 +1,18 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type CreateFocusAreaRequestDTO struct {
 	Name string `json:"name"`
 }
 
 func (dto *CreateFocusAreaRequestDTO) Bind(r *http.Request) error {
+	if dto.Name == "" {
+		return fmt.Errorf("name is required")
+	}
+
 	return nil
 }
